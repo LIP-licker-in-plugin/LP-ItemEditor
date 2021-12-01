c989531937810cce1153445f34b2f15f35c9f378
darksoldier1404 <darksoldier1404@gmail.com>
2021-12-01 11:10:10 +0900

    DUC 설치 확인 추가

--- src/main/java/com/darksoldier1404/die/ItemEditor.java
+++ src/main/java/com/darksoldier1404/die/ItemEditor.java
@@ -1,15 +1,18 @@
 package com.darksoldier1404.die;
 
 import com.darksoldier1404.die.commands.DIECommand;
+import com.darksoldier1404.duc.UniversalCore;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemEditor extends JavaPlugin {
+    private UniversalCore core;
     public static String prefix = "§f[ §6DIE §f] ";
     private static ItemEditor plugin;
     public static List<String> materials = new ArrayList<>();
@@ -22,6 +25,13 @@ public class ItemEditor extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+        Plugin pl = getServer().getPluginManager().getPlugin("DP-UniversalCore");
+        if(pl == null) {
+            getLogger().warning("DP-UniversalCore 플러그인이 설치되어있지 않습니다.");
+            plugin.setEnabled(false);
+            return;
+        }
+        core = (UniversalCore) pl;
         getCommand("die").setExecutor(new DIECommand());
         Bukkit.getScheduler().runTask(plugin, this::initMaterialList);
     }
