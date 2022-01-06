44a3da9b069bc7f316031d49110d39b8dfe703ca
darksoldier1404 <darksoldier1404@gmail.com>
2022-01-07 01:21:23 +0900

    NBT 커맨드 업데이트, name 커맨드 space 버그 수정

--- v1_14_4/src/main/java/com/darksoldier1404/die/functions/DIEFunction.java
+++ v1_14_4/src/main/java/com/darksoldier1404/die/functions/DIEFunction.java
@@ -14,6 +14,10 @@ public class DIEFunction {
         while (i.hasNext()) {
             s.append(i.next()).append(" ");
         }
+        // delete last space
+        if(s.charAt(s.length()-1) == ' ') {
+            s.deleteCharAt(s.length()-1);
+        }
         return ChatColor.translateAlternateColorCodes('&', s.toString());
     }
 }
